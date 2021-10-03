21015868a55c9a267dfaddf460f1c91b5cd34b6b
Monique Tickle <270032361@bun2-206-24.acg.ac.nz>
2021-10-04 12:18:09 +1300

    day 1 php basics lession :star:

---
+++ php/04oct-helloworld/arrays.php
@@ -0,0 +1,60 @@
+  <h1>arrays!</h1>
+<?php
+$cars = array('big car', 'little car', 'normal car');
+print_r($cars);
+echo '<br>the third car is ' . $cars[2];
+
+$people = array(
+  'joe' => 'blond',
+  'nancy' => 'brunette',
+  'dude' => 'red'
+);
+echo '<br>';
+print_r($people);
+echo '<br> their hair colour is ' . $people['nancy'];
+?>
+<h3>loop through array</h3>
+<?php
+
+foreach($people as $person) {
+   echo '<br>' . $person . '<br>';
+} ?>
+<!-- // creating a variable called person - saying go through this people array and make each item into a person -->
+
+<div class="row">
+<?php
+foreach($people as $key => $person) { ?>
+  <!-- closing the php bracket to dip into html then opening php within the html to echo the stuff and things -->
+  <div class="col-4">
+    <p><?php echo $key . ' has ' . $person . ' hair';?></p>
+  </div>
+  <!-- echo $key . ' has ' . $person . ' hair<br>'; -->
+  <?php
+}
+
+  $animals = array(
+    'cat' => 'small animal that thinks its a big feline',
+    'fox' => 'dog, but make it evil for fun',
+    'wolf' => 'dog, but make it bigger and awoo and all that'
+  );
+
+  foreach($animals as $key => $description) { ?>
+    <!-- closing the php bracket to dip into html then opening php within the html to echo the stuff and things -->
+    <div class="col-4">
+      <h3><?php echo 'THE ' . $key?></h3>
+      <p><?php echo $key . ' - ' . $description;?></p>
+    </div>
+    <!-- echo $key . ' has ' . $person . ' hair<br>'; -->
+    <?php
+  }
+?>
+</div>
+
+<h1>looplooploops</h1>
+<h6>NORMAL LOOP not FOREACH</h6>
+<?php
+  for ($i = 0; $i < 10; $i ++){
+    $number = 10 * $i;
+    echo 'new number alert! its now at ' . $number . '<br>';
+  }
+?>
---
+++ php/04oct-helloworld/first-exercise.php
@@ -0,0 +1,26 @@
+<!-- php can include html, css and js. wow -->
+
+<?php
+
+echo '<h1>hello world</h1>';
+echo 'heeey this is php wowwowow';
+
+?>
+
+<p>hello there <?php echo 'mo';?>!</p>
+
+<!-- php is a backend language.. very cool B) -->
+
+<h1>VARIABLES!</h1>
+
+<?php
+  $new_var = 1;
+  $name = 'mo';
+  echo 'my variable is ' . $new_var . '<br>';
+  echo 'my name is' . ' ' . $name;
+  // use . to concatenate like you would use a + in js
+?>
+
+<!-- php doesnt give console errors,, so be careful -->
+
+<!-- include is similar to js imports- allows you to attach another file into ur project wow -->
